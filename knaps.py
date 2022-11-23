695830ccf82f9fa60b2ea78744c5c3edef752c44
Theresia028 <83936446+Theresia028@users.noreply.github.com>
2022-11-23 19:01:02 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,136 +1,188 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
 from collections import OrderedDict
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import BaggingClassifier
-from sklearn.datasets import make_classification
-from sklearn.svm import SVC
 
-st.write(""" 
-# Cek data
+#Metrics
+from sklearn.metrics import make_scorer, accuracy_score,precision_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score ,precision_score,recall_score,f1_score
+
+#Model Select
+from sklearn.model_selection import KFold,train_test_split,cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import  LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import linear_model
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn import svm
+from sklearn import metrics 
+
+st.title(' Aplikasi Web Data Mining')
+st.write("""
+### Klasifikasi efek Covid 19 pada liver menggunakan Metode Decision tree, Random forest, dan SVM
 """)
+st.sidebar.write("""
+            ### Pilih Metode yang anda inginkan :"""
+            )
+algoritma=st.sidebar.selectbox(
+    'Pilih', ('Decision Tree','Random Forest','SVM')
+)
 
-st.write("=========================================================================")
+st.write('## 2. About Dataset (Heart Failure)')
+data_hf = pd.read_csv("https://raw.githubusercontent.com/Theresia028/pendatweb1/main/covid-liver.csv")
+st.write("Dataset Covid-19 Liver : (https://raw.githubusercontent.com/Theresia028/pendatweb1/main/covid-liver.csv) ", data_hf)
 
-tab1, tab2, tab3, tab4 = st.tabs(["Import Data", "Preprocessing", "Modelling", "Evalutions"])
+st.write('Dataset Description :')
+st.write('1. Cancer: Age of the patient')
+st.write('2. Year: Haemoglobin level of patient')
+st.write('3. Month: Level of the CPK enzyme in the blood (mcg/L)')
+st.write('4. Bleed: If the patient has diabetes (Boolean)')
+st.write('5. Mode_Presentation: Percentage of blood leaving the heart at each contraction')
+st.write('6. Age: If the patient has hypertension(Boolean)')
+st.write('7. Gender: Platelet count of blood (kiloplatelets/mL)')
+st.write('8. Etiology: Level of serum creatinine in the blood (mg/dL)')
+st.write('9. Cirrhosis: Level of serum sodium in the blood (mEq/L)')
+st.write('10. Size: Sex of the patient(Boolean)')
+st.write('11. HCC_TNM_Stage: If the patient smokes or not(Boolean)')
+st.write('12. HCC_BCLC_Stage: Follow-up period')
+st.write('13. ICC_TNM_Stage: If the patient deceased during the follow-up period')
+ 
+st.write('Jumlah baris dan kolom :', data_hf.shape)
 
-with tab1:
-    st.write("Import Data")
-    data = pd.read_csv("https://raw.githubusercontent.com/Feb11F/dataset/main/credit_score.csv")
-    st.dataframe(data)
+X=data_hf.iloc[:,0:12].values 
+y=data_hf.iloc[:,12].values
 
-with tab2:
-    data.head()
+st.write('Jumlah kelas : ', len(np.unique(y)))
 
-    X = data.drop(columns=["risk_rating"])
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
 
-    X.head()
 
-    # Mengambil kolom Rata-rata overdue dan mentranformasi menggunakan one-hot encoding
-    rata_overdue = pd.get_dummies(X["rata_rata_overdue"], prefix="overdue")
-    X = X.join(rata_overdue)
+#Train and Test split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=0)
 
-    X = X.drop(columns="rata_rata_overdue")
+# Decision Tree
+decision_tree = DecisionTreeClassifier() 
+decision_tree.fit(X_train, y_train)  
+Y_pred = decision_tree.predict(X_test) 
+accuracy_dt=round(accuracy_score(y_test,Y_pred)* 100, 2)
+acc_decision_tree = round(decision_tree.score(X_train, y_train) * 100, 2)
 
-    labels = data["risk_rating"]
-    # 
-    KPR_status = pd.get_dummies(X["kpr_aktif"], prefix="KPR")
-    X = X.join(KPR_status)
+cm = confusion_matrix(y_test, Y_pred)
+accuracy = accuracy_score(y_test,Y_pred)
+precision =precision_score(y_test, Y_pred,average='micro')
+recall =  recall_score(y_test, Y_pred,average='micro')
+f1 = f1_score(y_test,Y_pred,average='micro')
+print('Confusion matrix for DecisionTree\n',cm)
+print('accuracy_DecisionTree: %.3f' %accuracy)
+print('precision_DecisionTree: %.3f' %precision)
+print('recall_DecisionTree: %.3f' %recall)
+print('f1-score_DecisionTree : %.3f' %f1)
 
-    # remove "rata_rata_overdue" feature
-    X = X.drop(columns = "kpr_aktif")
+# Random Forest
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train, y_train)
+Y_prediction = random_forest.predict(X_test)
+accuracy_rf=round(accuracy_score(y_test,Y_prediction)* 100, 2)
+acc_random_forest = round(random_forest.score(X_train, y_train) * 100, 2)
 
-    st.write("Menampilkan dataframe yang rata-rata overdue, risk rating dan kpr aktif sudah di drop")
-    st.dataframe(X)
 
-    st.write(" ## Normalisasi")
-    st.write("Normalize feature 'pendapatan_setahun_juta', 'durasi_pinjaman_bulan', 'jumlah_tanggungan'")
-    old_normalize_feature_labels = ['pendapatan_setahun_juta', 'durasi_pinjaman_bulan', 'jumlah_tanggungan']
-    new_normalized_feature_labels = ['norm_pendapatan_setahun_juta', 'norm_durasi_pinjaman_bulan', 'norm_jumlah_tanggungan']
-    normalize_feature = data[old_normalize_feature_labels]
+cm = confusion_matrix(y_test, Y_prediction)
+accuracy = accuracy_score(y_test,Y_prediction)
+precision =precision_score(y_test, Y_prediction,average='micro')
+recall =  recall_score(y_test, Y_prediction,average='micro')
+f1 = f1_score(y_test,Y_prediction,average='micro')
+print('Confusion matrix for Random Forest\n',cm)
+print('accuracy_random_Forest : %.3f' %accuracy)
+print('precision_random_Forest : %.3f' %precision)
+print('recall_random_Forest : %.3f' %recall)
+print('f1-score_random_Forest : %.3f' %f1)
 
-    st.dataframe(normalize_feature)
+#SVM
+SVM = svm.SVC(kernel='linear') 
+SVM.fit(X_train, y_train)
+Y_prediction = SVM.predict(X_test)
+accuracy_SVM=round(accuracy_score(y_test,Y_pred)* 100, 2)
+acc_SVM = round(SVM.score(X_train, y_train) * 100, 2)
 
-    scaler = MinMaxScaler()
+cm = confusion_matrix(y_test, Y_pred)
+accuracy = accuracy_score(y_test,Y_pred)
+precision =precision_score(y_test, Y_pred,average='micro')
+recall =  recall_score(y_test, Y_pred,average='micro')
+f1 = f1_score(y_test,Y_pred,average='micro')
+print('Confusion matrix for SVM\n',cm)
+print('accuracy_SVM : %.3f' %accuracy)
+print('precision_SVM : %.3f' %precision)
+print('recall_SVM : %.3f' %recall)
+print('f1-score_SVM : %.3f' %f1)
 
-    scaler.fit(normalize_feature)
+st.write('## 3. Akurasi Metode')
 
-    normalized_feature = scaler.transform(normalize_feature)
+results = pd.DataFrame({
+    'Model': ['Decision Tree','Random Forest','SVM'],
+    'Accuracy_score':[accuracy_dt,
+                      accuracy_rf,accuracy_SVM
+                     ]})
+result_df = results.sort_values(by='Accuracy_score', ascending=False)
+result_df = result_df.reset_index(drop=True)
+result_df.head(9)
+st.write(result_df)
 
-    normalized_feature_df = pd.DataFrame(normalized_feature, columns = new_normalized_feature_labels)
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.bar(['Decision Tree', 'Random Forest','SVM'],[accuracy_dt, accuracy_rf, accuracy_SVM])
+plt.show()
+st.pyplot(fig)
 
-    st.write("Data setelah dinormalisasi")
-    st.dataframe(normalized_feature_df)
+age = st.sidebar.number_input("umur =", min_value=40 ,max_value=90)
+anemia = st.sidebar.number_input("anemia =", min_value=0, max_value=1)
+creatinine_phosphokinase = st.sidebar.number_input("creatinine_phosphokinase =", min_value=0 , max_value=10000)
+diabetes = st.sidebar.number_input("diabetes =", min_value=0, max_value=1)
+ejection_fraction = st.sidebar.number_input("ejection_fraction =", min_value=0, max_value=100)
+high_blood_pressure = st.sidebar.number_input("high_blood_pressure =", min_value=0 ,max_value=1)
+platelets = st.sidebar.number_input("platelets =", min_value=100000, max_value=1000000)
+serum_creatinine = st.sidebar.number_input("serum_creatinine =", min_value=0, max_value=10)
+serum_sodium = st.sidebar.number_input("serum_sodium =", min_value=100, max_value=150)
+sex = st.sidebar.number_input("sex =", min_value=0, max_value=1)
+smoking = st.sidebar.number_input("smoking =", min_value=0, max_value=1)
+time = st.sidebar.number_input("time =", min_value=1, max_value=500)
+submit = st.sidebar.button("Submit")
 
-    X = X.drop(columns = old_normalize_feature_labels)
-
-    X = X.join(normalized_feature_df)
-
-    X = X.join(labels)
-
-    st.write("Dataframe X baru")
-    st.dataframe(X)
-
-    subject_lables = ["Unnamed: 0",  "kode_kontrak"]
-    X = X.drop(columns = subject_lables)
-
-    # percent_amount_of_test_data = / HUNDRED_PERCENT
-    percent_amount_of_test_data = 0.3
-
-    st.write("Dataframe X baru yang tidak ada fitur/kolom unnamed: 0 dan kode kontrak")
-    st.dataframe(X)
-    st.write("## Hitung Data")
-    st.write("- Pisahkan kolom risk rating dari data frame")
-    st.write("- Ambil kolom 'risk rating' sebagai target kolom untuk kategori kelas")
-    st.write("- Pisahkan data latih dengan data tes")
-    st.write("""            Spliting Data
-
-                data latih (nilai data)
-                X_train 
-
-                data tes (nilai data)
-                X_test 
-
-                data latih (kelas data)
-                y_train
-
-                data tes (kelas data)
-                y_test""")
-
-    # separate target 
-
-    # values
-    matrices_X = X.iloc[:,0:10].values
-
-    # classes
-    matrices_Y = X.iloc[:,10].values
-
-    X_1 = X.iloc[:,0:10].values
-    Y_1 = X.iloc[:, -1].values
-
-    # X_train, X_test, y_train, y_test = train_test_split(matrices_X, matrices_Y, test_size = percent_amount_of_test_data, random_state=0)
-    X_train, X_test, y_train, y_test = train_test_split(X_1, Y_1, test_size = percent_amount_of_test_data, random_state=0)
-
-    st.write("Menampilkan Y_1")
-    st.write(Y_1)
-    
-    st.write("Menampilkan X_1")
-    st.write(X_1)
-    ### Dictionary to store model and its accuracy
-
-    model_accuracy = OrderedDict()
-
-    ### Dictionary to store model and its precision
-
-    model_precision = OrderedDict()
-
-    ### Dictionary to store model and its recall
-
-    model_recall = OrderedDict()
+if submit :
+    if algoritma == 'Decision Tree' :
+        X_new = np.array([[age,anemia,creatinine_phosphokinase,diabetes,ejection_fraction,high_blood_pressure,platelets,serum_creatinine,serum_sodium,
+        sex,smoking,time]])
+        prediksi = decision_tree.predict(X_new)
+        if prediksi == 1 :
+            st.sidebar.write(""" # Hasil Prediksi :
+             rekiso meninggal tinggi""")
+        else : 
+            st.sidebar.write("""# Hasil Prediksi :
+             rekiso meninggal rendah""")
+    elif algoritma == 'Random Forest' :
+        X_new = np.array([[age,anemia,creatinine_phosphokinase,diabetes,ejection_fraction,high_blood_pressure,platelets,serum_creatinine,serum_sodium,
+        sex,smoking,time]])
+        prediksi = random_forest.predict(X_new)
+        if prediksi == 1 :
+            st.sidebar.write("""# Hasil Prediksi :
+            rekiso meninggal tinggi""")
+        else : 
+            st.sidebar.write("""# Hasil Prediksi :
+            rekiso meninggal rendah""")
+    else :
+        X_new = np.array([[age,anemia,creatinine_phosphokinase,diabetes,ejection_fraction,high_blood_pressure,platelets,serum_creatinine,serum_sodium,
+        sex,smoking,time]])
+        prediksi = SVM.predict(X_new)
+        if prediksi == 1 :
+            st.sidebar.write("""# Hasil Prediksi :
+            rekiso meninggal tinggi""")
+        else : 
+            st.sidebar.write("""# Hasil Prediksi :
+            rekiso meninggal rendah""")

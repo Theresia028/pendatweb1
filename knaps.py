365c14fcbbffe1202432b67bf14f9a0a4dfa272d
Theresia028 <83936446+Theresia028@users.noreply.github.com>
2022-12-05 19:25:24 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -16,7 +16,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 st.title("PENAMBANGAN DATA C")
 
-data_set_description, upload_data, preprocessing, modeling, implementation = st.tabs(["Deskripsi DataSet", "DataSet", "Preprocessing", "Modeling", "Implementation"])
+data_set_description, upload_data, preprocessing, modeling, implementation = st.tabs(["DESKRIPSI DATASET", "DATASET", "PREPROCESSING", "MODELING", "IMPLEMENTATION"])
 
 with data_set_description:
     st.write("##### Nama  : Theresia Nazela ")
